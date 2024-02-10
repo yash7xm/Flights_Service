f4cbb3a8ea45d104d9073d13c9e4805a790ac57c
yash7xm <yash.7xm@gmail.com>
2024-02-11 02:04:30 +0530

    middleware for city added

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
 const { CityController } = require("../../controllers");
+const { CityMiddlewaers } = require("../../middlewares");
 const router = express.Router();
 
 // /api/v1/cities POST
-router.post("/", CityController.createCity);
+router.post(
+  "/",
+  CityMiddlewaers.validateCreateRequest,
+  CityController.createCity
+);
 
 module.exports = router;
