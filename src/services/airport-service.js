31556c4c5f320dd2ebda07c3af06d6973d4b5be3
yash7xm <yash.7xm@gmail.com>
2024-02-12 17:05:03 +0530

    airport services added

---
+++ src/services/airport-service.js
@@ -0,0 +1,100 @@
+const { StatusCodes } = require("http-status-codes");
+
+const { AirportRepository } = require("../repositories");
+const AppError = require("../utils/errors/app-error");
+
+const airportRepository = new AirportRepository();
+
+async function createAirport(data) {
+  try {
+    const airport = await airportRepository.create(data);
+    return airport;
+  } catch (error) {
+    if (error.name == "SequelizeValidationError") {
+      let explanation = [];
+      error.errors.forEach((err) => {
+        explanation.push(err.message);
+      });
+
+      throw new AppError(explanation, StatusCodes.BAD_REQUEST);
+    }
+    throw new AppError(
+      "Cannot create a new Airport object",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getAirports() {
+  try {
+    const airport = await airportRepository.getAll();
+    return airport;
+  } catch (error) {
+    throw new AppError(
+      "Cannot fetch data of all the airports",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getAirport(id) {
+  try {
+    const airport = await airportRepository.get(id);
+    return airport;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The airport you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of the requested airport",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function destroyAirport(id) {
+  try {
+    const airport = await airportRepository.destroy(id);
+    return airport;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The airport you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot delete data of the requested airport",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function updateAirport(id, data) {
+  try {
+    const airport = await airportRepository.update(id, data);
+    return airport;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The airport you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot update data of the requested airport",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+module.exports = {
+  createAirport,
+  getAirports,
+  getAirport,
+  destroyAirport,
+  updateAirport,
+};
