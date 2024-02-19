0b870a971d431ab25c0477220c18b3af2d115693
yash7xm <yash.7xm@gmail.com>
2024-02-19 16:14:27 +0530

    added get flight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -11,6 +11,10 @@ router.post(
   FlightController.createFlight
 );
 
+// /api/v1/flights?trips=MUM-DEL GET
 router.get("/", FlightController.getAllFlights);
+
+// /api/v1/flights/:id GET
+router.get("/:id", FlightController.getFlight);
 
 module.exports = router;
